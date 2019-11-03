23b2cf18c3ee557498630522de03e7ff9f03ffd6
vgandari <victor.gandarillas@icloud.com>
2019-11-03 14:26:45 -0800

    cleanup

--- generate_nodes.m
+++ generate_nodes.m
@@ -1,14 +1,13 @@
 function [num_nodes, n_mag, n_phase] = generate_nodes(order, beta, phi, r0)
     % Scale factors for node radial positions and member lengths
     % TODO: test a = 1; expect to generate secants
-    a         = sin(beta)/(sin(beta+phi));
-    c         = sin(phi)/(sin(beta+phi));
+    a         = sin(beta)/sin(beta+phi);
 
     % Generate node radial positions and member lengths
     radii          = zeros(order,1);
     radii(1)       = r0;
-    for l=0:order-1
-        radii(l+2) = a*radii(l+1);
+    for l=1:order
+        radii(l+1) = a*radii(l);
     end
 
     % Set up magnitudes and phases of members of a Mitchell Spiral
